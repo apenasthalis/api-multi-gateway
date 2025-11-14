dedd17a1388160e49fc9c61b0fbb33142d480292
thalis <1thalisgabriel1@gmail.com>
2025-11-14 12:24:39 -0300

    feat(http): request, response, route, controller

---
+++ src/Http/Request.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http;
+
+class Request
+{
+    public static function method()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public static function body()
+    {
+        $json = json_decode(file_get_contents("php://input"), true) ?? [];
+        $data = match(self::method()) {
+            'GET' => $_GET,
+            'POST', 'PUT', 'DELETE' => $json,
+        };
+
+        return $data;
+    }
+
+    public static function authorization()
+    {
+        $authorization = getallheaders();
+        
+        if (!isset($authorization['Authorization'])) return ['error' => 'Sorry, no authorization header provided'];
+        
+        $authorizationPartials = explode(' ', $authorization['Authorization']);
+
+        return $authorizationPartials[1] ?? '';
+    } 
+}
